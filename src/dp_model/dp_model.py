0daecd1fafda382e2a61b504976da7cb180d5b81
leemq <meow_bot@hotmail.com>
2022-07-01 03:25:41 +0800

    [feature] working DP model

--- src/dp_model/dp_model.py
+++ src/dp_model/dp_model.py
@@ -185,4 +185,35 @@ class DpModel:
         best_state, best_state_value = min(end_states.items(), key=self._compare_states)
         return best_state, best_state_value
 
+    def trace_best_states(self) -> List[Tuple[State, StateValue]]:
+        best_state, best_state_value = self.get_best_end_state()
+        reverse_list = list()
+        reverse_list.append((best_state, best_state_value))
+        current_state = best_state
+        current_value = best_state_value
+        while current_state.period > 0:
+            next_state = current_value.origin
+            next_value = self.states[current_state.period-1][next_state]
+            reverse_list.append((next_state, next_value))
+            current_state, current_value = next_state, next_value
+        state_list = reversed(reverse_list)
+        return list(state_list)
+
+    def get_best_state_df(self) -> pd.DataFrame:
+        best_actions = self.trace_best_states()
+        state_list = list()
+        for state, state_value in best_actions:
+            state_dict = dict()
+            state_dict['period'] = state.period
+            state_dict['active_tank'] = state.active_tank
+            for tank_state in state.tank_states:
+                tank_id = tank_state.id
+                state_dict[f'current_slurry_unit_{tank_id}'] = tank_state.current_slurry_unit
+                state_dict[f'cleaning_period_remaining_{tank_id}'] = tank_state.cleaning_period_remaining
+                state_dict[f'filling_period_remaining_{tank_id}'] = tank_state.filling_period_remaining
+                state_dict[f'down_start_period_{tank_id}'] = tank_state.down_start_period
+            state_dict['cost'] = state_value.cost
+            state_list.append(state_dict)
+        state_df = pd.DataFrame(state_list)
+        return state_df
 
--- src/dp_model/dp_model_test.py
+++ src/dp_model/dp_model_test.py
@@ -1,11 +1,35 @@
 import pytest
 
 from src.incoming_wip.incoming_wip_generator import IncomingWipGenerator
-from src.incoming_wip.incoming_wip_config import default_incoming_wip_config
+from src.incoming_wip.incoming_wip_config import default_incoming_wip_config, IncomingWipConfig
 
 from src.dp_model.dp_model_config import default_dp_model_config
 from src.dp_model.dp_model import DpModel
+from src.scenario.scenario_config import default_scenario_config
 
-incoming_wip = IncomingWipGenerator(default_incoming_wip_config).generate()
-dp_model = DpModel(default_dp_model_config)
-dp_model.run(incoming_wip)
+
+def randomized_incoming_wip_config():
+    config_list = list()
+    for i in range(7):
+        incoming_wip_config = IncomingWipConfig(
+            scenario=default_scenario_config,
+            rand_seed=i,
+            min_wip_cluster=1,
+            max_wip_cluster=4,
+            max_wip_magnitude=200,
+            wafer_slurry_consumption_kg=3,
+        )
+        config_list.append(incoming_wip_config)
+    return config_list
+
+
+@pytest.mark.parametrize('config', randomized_incoming_wip_config())
+def test_dp_model(config):
+    incoming_wip = IncomingWipGenerator(config).generate()
+    dp_model = DpModel(default_dp_model_config)
+    dp_model.run(incoming_wip)
+    state_df = dp_model.get_best_state_df()
+    best_end_state = dp_model.get_best_end_state()
+    print(state_df)
+    assert best_end_state[1].cost == 0
+
